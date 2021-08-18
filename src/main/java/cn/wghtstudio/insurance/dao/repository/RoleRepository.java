f514162e5ae822c6873a26ab3243bc5971df7682
wangjq4214 <w1637894214@163.com>
2021-08-18 16:41:27 +0800

    feat: 添加级联查询得到用户权限

--- src/main/java/cn/wghtstudio/insurance/dao/repository/RoleRepository.java
+++ src/main/java/cn/wghtstudio/insurance/dao/repository/RoleRepository.java
@@ -1,13 +1,11 @@
 package cn.wghtstudio.insurance.dao.repository;
 
-import cn.wghtstudio.insurance.dao.entity.User;
+import cn.wghtstudio.insurance.dao.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
-public interface UserRepository {
-    User getUserByUsername(String username);
-
-    User getUserByID(int id);
+public interface RoleRepository {
+    Role getRoleByID(int id);
 }
