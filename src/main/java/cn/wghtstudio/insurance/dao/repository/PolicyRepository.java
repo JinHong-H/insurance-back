f2747c49bd523083c562ca7c85a5c15ce1038f00
Monkeyman <2918508198@qq.com>
2021-08-28 14:51:55 +0800

    订单匹配

--- src/main/java/cn/wghtstudio/insurance/dao/repository/PolicyRepository.java
+++ src/main/java/cn/wghtstudio/insurance/dao/repository/PolicyRepository.java
@@ -3,9 +3,13 @@ package cn.wghtstudio.insurance.dao.repository;
 import cn.wghtstudio.insurance.dao.entity.Policy;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PolicyRepository {
     void createPolicy(Policy policy);
 
     void updatePolicy(Policy policy);
+    
+    List<Policy> selectPolicyByorderid(Integer orderID);
 }
--- src/main/java/cn/wghtstudio/insurance/service/impl/OcrInfoImpl.java
+++ src/main/java/cn/wghtstudio/insurance/service/impl/OcrInfoImpl.java
@@ -12,9 +12,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
@@ -22,13 +27,14 @@ import java.util.zip.ZipInputStream;
 
 class PolicyDealImpl implements Runnable {
     private final String filename;
-
+    
     private final byte[] file;
-
+    
     private final PolicyRepository policyRepository;
-
+    private final DrivingLicenseRepository drivingLicenseRepository;
+    
     private Integer policyId;
-
+    
     private String getInsuranceNumber(String words) {
         Pattern pattern = Pattern.compile("保险单号\\s?[:：]\\s?([A-Z]{4}[0-9]{18})");
         Matcher matcher = pattern.matcher(words);
@@ -37,7 +43,7 @@ class PolicyDealImpl implements Runnable {
         }
         return null;
     }
-
+    
     private String getPlateNumber(String words) {
         Pattern pattern = Pattern.compile("车牌号\\s?[:：]\\s?([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼](([A-HJ-Z][A-HJ-NP-Z0-9]{5})|([A-HJ-Z](([DF][A-HJ-NP-Z0-9][0-9]{4})|([0-9]{5}[DF])))|([A-HJ-Z][A-D0-9][0-9]{3}警)))|([0-9]{6}使)|((([沪粤川云桂鄂陕蒙藏黑辽渝]A)|鲁B|闽D|蒙E|蒙H)[0-9]{4}领)|(WJ[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼·•][0-9]{4}[TDSHBXJ0-9])|([VKHBSLJNGCE][A-DJ-PR-TVY][0-9]{5})");
         Matcher matcher = pattern.matcher(words);
@@ -46,7 +52,7 @@ class PolicyDealImpl implements Runnable {
         }
         return null;
     }
-
+    
     private String getFrameNumber(String words) {
         Pattern pattern = Pattern.compile("车架号\\s?[:：]\\s?([A-Z]{3}[A-z0-9]{14})");
         Matcher matcher = pattern.matcher(words);
@@ -55,7 +61,7 @@ class PolicyDealImpl implements Runnable {
         }
         return null;
     }
-
+    
     private String getEngineNumber(String words) {
         Pattern pattern = Pattern.compile("发动机号\\s?[:：]\\s?([A-Z0-9]{6,11})");
         Matcher matcher = pattern.matcher(words);
@@ -64,7 +70,7 @@ class PolicyDealImpl implements Runnable {
         }
         return null;
     }
-
+    
     private void createItemInDB() {
         Policy policy = Policy.builder().
                 url("https://versicherung.oss-cn-beijing.aliyuncs.com/" + filename).
@@ -72,38 +78,38 @@ class PolicyDealImpl implements Runnable {
         policyRepository.createPolicy(policy);
         policyId = policy.getId();
     }
-
+    
     private void putPolicyToOSS() {
         AliyunUtil.putObject(filename, new ByteArrayInputStream(file));
     }
-
+    
     private void doOCR() throws IOException, OCRException {
         String b64encoded = Base64.getEncoder().encodeToString(file);
         final String token = GetOcrToken.getAuthToken();
-
+        
         final InsurancePolicyResponse response = OcrInfoGetter.vehicleInsurance(b64encoded, token);
         final List<InsurancePolicyResponse.WordsResult> wordsResultList = response.getWordsResult();
-
+        
         String number = null, plateNumber = null, frame = null, engine = null;
         for (InsurancePolicyResponse.WordsResult wordsResult : wordsResultList) {
             String words = wordsResult.getWords();
             if (number == null) {
                 number = getInsuranceNumber(words);
             }
-
+            
             if (plateNumber == null) {
                 plateNumber = getPlateNumber(words);
             }
-
+            
             if (frame == null) {
                 frame = getFrameNumber(words);
             }
-
+            
             if (engine == null) {
                 engine = getEngineNumber(words);
             }
         }
-
+        
         if (policyId != null) {
             Policy policy = Policy.builder().
                     id(policyId).
@@ -111,14 +117,37 @@ class PolicyDealImpl implements Runnable {
                     build();
             policyRepository.updatePolicy(policy);
         }
+        List<DrivingLicense> res = drivingLicenseRepository.getDrivingLicenseByPolicyinfo(DrivingLicense.builder().
+                engine(engine).frame(frame)
+                .plateNumber(plateNumber).build());
+        if (res.size() == 0) {
+            throw new OCRException();
+        }
+        if (res.size() == 1) {
+            Policy policy = Policy.builder().
+                    orderId(res.get(0).getOrderId()).
+                    build();
+            policyRepository.updatePolicy(policy);
+        }
+        if (res.size() >= 2) {
+            for (DrivingLicense temp : res) {
+                List<Policy> tempres = policyRepository.selectPolicyByorderid(temp.getOrderId());
+                if (tempres.size() == 0) {
+                    Policy policy = Policy.builder().id(policyId).orderId(temp.getOrderId()).build();
+                    policyRepository.updatePolicy(policy);
+                    break;
+                }
+            }
+        }
     }
-
-    public PolicyDealImpl(String filename, byte[] file, PolicyRepository policyRepository) {
+    
+    public PolicyDealImpl(String filename, byte[] file, PolicyRepository policyRepository, DrivingLicenseRepository drivingLicenseRepository) {
         this.filename = filename;
         this.file = file;
         this.policyRepository = policyRepository;
+        this.drivingLicenseRepository = drivingLicenseRepository;
     }
-
+    
     @Override
     public void run() {
         try {
@@ -135,22 +164,22 @@ class PolicyDealImpl implements Runnable {
 public class OcrInfoImpl implements OcrInfoService {
     @Resource
     private IdCardRepository idCardRepository;
-
+    
     @Resource
     private BusinessLicenseRepository businessLicenseRepository;
-
+    
     @Resource
     private DrivingLicenseRepository drivingLicenseRepository;
-
+    
     @Resource
     private CertificateRepository certificateRepository;
-
+    
     @Resource
     private PolicyRepository policyRepository;
-
+    
     @Resource
     private OtherFileRepository otherFileRepository;
-
+    
     private final ExecutorService executorService = new ThreadPoolExecutor(
             4,
             10,
@@ -158,79 +187,43 @@ public class OcrInfoImpl implements OcrInfoService {
             TimeUnit.SECONDS,
             new SynchronousQueue<>()
     );
-
+    
     private String getPolicyName(String originName) {
         final String[] splitNames = originName.split("\\.");
         return splitNames[splitNames.length - 2] + UUID.randomUUID() + ".pdf";
     }
-
+    
     private Map<String, byte[]> unzipFile(MultipartFile file) throws IOException {
         Map<String, byte[]> streamMap = new HashMap<>();
         ZipInputStream zipInputStream = new ZipInputStream(file.getInputStream());
         ZipEntry zipEntry;
-
+        
         while ((zipEntry = zipInputStream.getNextEntry()) != null && !zipEntry.isDirectory()) {
             String name = "policy/" + getPolicyName(zipEntry.getName());
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
+            
             byte[] bytes = new byte[1024];
             int len;
             while ((len = zipInputStream.read(bytes)) != -1) {
                 byteArrayOutputStream.write(bytes, 0, len);
             }
-
+            
             byteArrayOutputStream.flush();
             byteArrayOutputStream.close();
             zipInputStream.closeEntry();
             streamMap.put(name, byteArrayOutputStream.toByteArray());
         }
-
+        
         zipInputStream.close();
-
+        
         return streamMap;
     }
-
-//    private String getInsuranceNumber(String words) {
-//        Pattern pattern = Pattern.compile("保险单号\\s?[:：]\\s?([A-Z]{4}[0-9]{18})");
-//        Matcher matcher = pattern.matcher(words);
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//        return null;
-//    }
-//
-//    private String getPlateNumber(String words) {
-//        Pattern pattern = Pattern.compile("车牌号\\s?[:：]\\s?([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼](([A-HJ-Z][A-HJ-NP-Z0-9]{5})|([A-HJ-Z](([DF][A-HJ-NP-Z0-9][0-9]{4})|([0-9]{5}[DF])))|([A-HJ-Z][A-D0-9][0-9]{3}警)))|([0-9]{6}使)|((([沪粤川云桂鄂陕蒙藏黑辽渝]A)|鲁B|闽D|蒙E|蒙H)[0-9]{4}领)|(WJ[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼·•][0-9]{4}[TDSHBXJ0-9])|([VKHBSLJNGCE][A-DJ-PR-TVY][0-9]{5})");
-//        Matcher matcher = pattern.matcher(words);
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//        return null;
-//    }
-//
-//    private String getFrameNumber(String words) {
-//        Pattern pattern = Pattern.compile("车架号\\s?[:：]\\s?([A-Z]{3}[A-z0-9]{14})");
-//        Matcher matcher = pattern.matcher(words);
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//        return null;
-//    }
-//
-//    private String getEngineNumber(String words) {
-//        Pattern pattern = Pattern.compile("发动机号\\s?[:：]\\s?([A-Z0-9]{6,11})");
-//        Matcher matcher = pattern.matcher(words);
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//        return null;
-//    }
-
+    
     @Override
     public IdCardResponseBody idCardInfoService(String url) throws IOException, OCRException {
         final String token = GetOcrToken.getAuthToken();
         final IdCardResponse response = OcrInfoGetter.idCard(url, token);
-
+        
         final IdCardResponse.WordsResult wordsResult = response.getWordsResult();
         IdCard idCard = IdCard.builder().
                 url(url).
@@ -238,9 +231,9 @@ public class OcrInfoImpl implements OcrInfoService {
                 address(wordsResult.getAddress().getWords()).
                 number(wordsResult.getNumber().getWords()).
                 build();
-
+        
         idCardRepository.createIdCard(idCard);
-
+        
         return IdCardResponseBody.builder().
                 id(idCard.getId()).
                 name(idCard.getName()).
@@ -248,12 +241,12 @@ public class OcrInfoImpl implements OcrInfoService {
                 number(idCard.getNumber()).
                 build();
     }
-
+    
     @Override
     public BusinessLicenseResponseBody businessInfoService(String url) throws IOException, OCRException {
         final String token = GetOcrToken.getAuthToken();
         final BusinessResponse response = OcrInfoGetter.businessLicense(url, token);
-
+        
         final BusinessResponse.WordsResult wordsResult = response.getWordsResult();
         BusinessLicense businessLicense = BusinessLicense.builder().
                 url(url).
@@ -261,9 +254,9 @@ public class OcrInfoImpl implements OcrInfoService {
                 address(wordsResult.getAddress().getWords()).
                 number(wordsResult.getCreditCode().getWords()).
                 build();
-
+        
         businessLicenseRepository.createBusinessLicense(businessLicense);
-
+        
         return BusinessLicenseResponseBody.builder().
                 id(businessLicense.getId()).
                 name(businessLicense.getName()).
@@ -271,12 +264,12 @@ public class OcrInfoImpl implements OcrInfoService {
                 number(businessLicense.getNumber()).
                 build();
     }
-
+    
     @Override
     public DrivingLicenseResponseBody drivingInfoService(String url) throws IOException, OCRException {
         final String token = GetOcrToken.getAuthToken();
         final DrivingLicenseResponse response = OcrInfoGetter.vehicleLicense(url, token);
-
+        
         final DrivingLicenseResponse.WordsResult wordsResult = response.getWordsResult();
         DrivingLicense drivingLicense = DrivingLicense.builder().
                 url(url).
@@ -286,9 +279,9 @@ public class OcrInfoImpl implements OcrInfoService {
                 frame(wordsResult.getCarVerify().getWords()).
                 type(wordsResult.getModel().getWords()).
                 build();
-
+        
         drivingLicenseRepository.createDrivingLicense(drivingLicense);
-
+        
         return DrivingLicenseResponseBody.builder().
                 id(drivingLicense.getId()).
                 plate(drivingLicense.getPlateNumber()).
@@ -297,41 +290,41 @@ public class OcrInfoImpl implements OcrInfoService {
                 type(drivingLicense.getType()).
                 build();
     }
-
+    
     @Override
     public CertificateResponseBody certificateInfoService(String url) throws IOException, OCRException {
         final String token = GetOcrToken.getAuthToken();
         final CertificateResponse response = OcrInfoGetter.vehicleCertificate(url, token);
-
+        
         final CertificateResponse.WordsResult wordsResult = response.getWordsResult();
         Certificate certificate = Certificate.builder().
                 url(url).
                 engine(wordsResult.getEngineNo()).
                 frame(wordsResult.getVinNo()).
                 build();
-
+        
         certificateRepository.createCertificate(certificate);
-
+        
         return CertificateResponseBody.builder().
                 id(certificate.getId()).
                 engine(certificate.getEngine()).
                 frame(certificate.getFrame()).
                 build();
     }
-
+    
     @Override
     public OtherFileResponseBody otherFileService(String url) {
         OtherFile otherFile = OtherFile.builder().
                 url(url).
                 build();
-
+        
         otherFileRepository.createOtherFile(otherFile);
-
+        
         return OtherFileResponseBody.builder().
                 id(otherFile.getId()).
                 build();
     }
-
+    
     /**
      * 将保单任务提交到线程池中执行，经过写入数据库，识别生成投保单三个过程
      *
@@ -344,55 +337,26 @@ public class OcrInfoImpl implements OcrInfoService {
         if (originName == null) {
             throw new FileTypeException();
         }
-
+        
         final String[] splitNames = originName.split("\\.");
         // PDF 直接上传写入数据库
         if (splitNames[splitNames.length - 1].equals("pdf")) {
             String OSSPath = "policy/" + getPolicyName(originName);
-//            String b64encoded = Base64.getEncoder().encodeToString(file.getBytes());
-//            final String token = GetOcrToken.getAuthToken();
-//
-//            final InsurancePolicyResponse response = OcrInfoGetter.vehicleInsurance(b64encoded, token);
-//            final List<InsurancePolicyResponse.WordsResult> wordsResultList = response.getWordsResult();
-//            System.out.println(wordsResultList);
-//            String number = null, plateNumber = null, frame = null, engine = null;
-//            for (InsurancePolicyResponse.WordsResult wordsResult : wordsResultList) {
-//                String words = wordsResult.getWords();
-//                if (number == null) {
-//                    number = getInsuranceNumber(words);
-//                }
-//
-//                if (plateNumber == null) {
-//                    plateNumber = getPlateNumber(words);
-//                }
-//
-//                if (frame == null) {
-//                    frame = getFrameNumber(words);
-//                }
-//
-//                if (engine == null) {
-//                    engine = getEngineNumber(words);
-//                }
-//            }
-//            System.out.println(number);
-//            System.out.println(plateNumber);
-//            System.out.println(frame);
-//            System.out.println(engine);
-            executorService.submit(new PolicyDealImpl(OSSPath, file.getBytes(), policyRepository));
+            executorService.submit(new PolicyDealImpl(OSSPath, file.getBytes(), policyRepository, drivingLicenseRepository));
             return;
         }
-
+        
         // 将文件解压上传 Aliyun 或者直接上传，并写入数据库
         if (splitNames[splitNames.length - 1].equals("zip")) {
             Map<String, byte[]> streamMap = unzipFile(file);
             Set<Map.Entry<String, byte[]>> entrySet = streamMap.entrySet();
             for (Map.Entry<String, byte[]> entry : entrySet) {
-                executorService.submit(new PolicyDealImpl(entry.getKey(), entry.getValue(), policyRepository));
+                executorService.submit(new PolicyDealImpl(entry.getKey(), entry.getValue(), policyRepository, drivingLicenseRepository));
             }
-
+            
             return;
         }
-
+        
         throw new FileTypeException();
     }
 }
